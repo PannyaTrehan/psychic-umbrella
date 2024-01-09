1173f88efc413960a2e67250517d1c3e0e576ff9
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-01-10 00:13:39 +1100

    main files

---
+++ index.js
@@ -0,0 +1,86 @@
+const cells = document.querySelectorAll(".cell");
+const statusText = document.querySelector("#statusText");
+const restartBtn = document.querySelector("#restartBtn");
+
+let board = Array.from({ length: 6 }, () => Array(7).fill(''));
+let currentPlayer = "X";
+let running = false;
+
+initialiseGame();
+
+function initialiseGame() {
+    cells.forEach(cell => {
+        cell.addEventListener("click", cellClicked);
+        cell.addEventListener("mouseenter", cellHovered);
+        cell.addEventListener("mouseleave", cellUnhovered);
+    });
+    restartBtn.addEventListener("click", restartGame);
+    statusText.textContent = `${currentPlayer}'s turn`;
+    running = true;
+}
+
+function cellClicked() {
+    const clickedColumnIndex = this.getAttribute("cellIndex") % 7;
+    const cellIndex = findAvailableSlot(clickedColumnIndex);
+
+    if (isValidMove(cellIndex)) {
+        updateCell(cellIndex);
+        changePlayer();
+        checkWinner();
+    }
+}
+
+function cellHovered() {
+    const columnIndex = parseInt(this.getAttribute("cellIndex")) % 7;
+
+    for (let row = 0; row < 6; row++) {
+        cells[row * 7 + columnIndex].classList.add("hovered-cell");
+    }
+}
+
+function cellUnhovered() {
+    cells.forEach(cell => cell.classList.remove("hovered-cell"));
+}
+
+function findAvailableSlot(columnIndex) {
+    for (let row = 5; row >= 0; row--) {
+        if (board[row][columnIndex] === "") {
+            return row * 7 + columnIndex;
+        }
+    }
+}
+
+function isValidMove(index) {
+    return board[Math.floor(index / 7)][index % 7] === "" && running;
+}
+
+function updateCell(index) {
+    const cellToUpdate = document.querySelector(`[cellIndex="${index}"]`);
+    const row = Math.floor(index / 7);
+    const col = index % 7;
+
+    board[row][col] = currentPlayer;
+    cellToUpdate.textContent = currentPlayer;
+}
+
+function changePlayer() {
+    currentPlayer = (currentPlayer === "X") ? "O" : "X";
+    statusText.textContent = `${currentPlayer}'s turn`;
+}
+
+function restartGame() {
+    currentPlayer = "X";
+    board = Array.from({ length: 6 }, () => Array(7).fill(''));
+    statusText.textContent = `${currentPlayer}'s turn`;
+
+    cells.forEach(cell => {
+        cell.textContent = "";
+        cell.classList.remove("hovered-cell");
+    });
+
+    running = true;
+}
+
+function checkWinner() {
+    // TODO: Implement the logic to check for a winner in the 2D array (board)
+}
